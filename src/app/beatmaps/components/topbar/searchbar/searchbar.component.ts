9862dcc21e02e13812e33bb03032d50cbe0243e8
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-06 00:26:05 +0200

    Create searchbar component

---
+++ src/app/beatmaps/components/topbar/searchbar/searchbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+
+@Component({
+  selector: 'searchbar',
+  standalone: true,
+  imports: [
+    MatIcon
+  ],
+  templateUrl: './searchbar.component.html',
+  styleUrl: './searchbar.component.scss'
+})
+
+export class SearchbarComponent {
+
+}
