cae9c83019a7a58f063b50f7bedb4d84376355c6
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-06 06:32:02 +0200

    Added a way to determine if user is an admin

--- src/app/beatmaps/auth.service.ts
+++ src/app/beatmaps/auth.service.ts
@@ -4,57 +4,77 @@ import {ServicesService} from './services.service';
 import {Router} from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
-  private loggedIn = new BehaviorSubject<boolean>(false);
-  private avatarUrl = new BehaviorSubject<string>('./assets/icons/person.svg');
+    private loggedIn = new BehaviorSubject<boolean>(false);
+    private avatarUrl = new BehaviorSubject<string>('./assets/icons/person.svg');
+    private admin = new BehaviorSubject<boolean>(false);
 
-  constructor(private servicesService: ServicesService, private router: Router) {
-    if (typeof window !== 'undefined' && window.sessionStorage) {
-      const user = sessionStorage.getItem('user');
+    constructor(private servicesService: ServicesService, private router: Router) {
+        if (typeof window !== 'undefined' && window.sessionStorage) {
+            const osuUser = sessionStorage.getItem('osuUser');
 
-      if (user) {
-        this.loggedIn.next(true);
-        this.avatarUrl.next(JSON.parse(user).avatar_url);
-      }
+            if (osuUser) {
+                this.loggedIn.next(true);
+                this.avatarUrl.next(JSON.parse(osuUser).avatar_url);
+            }
+
+            const user = sessionStorage.getItem('user');
+            if (user) {
+                const userJson = JSON.parse(user);
+
+                userJson.roles.forEach((role: any) => {
+                    if (role.name === 'admin') {
+                        this.admin.next(true);
+                    }
+                })
+            }
+        }
     }
-  }
 
-  isLoggedIn(): Observable<boolean> {
-    return this.loggedIn.asObservable();
-  }
+    isLoggedIn(): Observable<boolean> {
+        return this.loggedIn.asObservable();
+    }
 
-  getAvatarUrl(): Observable<string> {
-    return this.avatarUrl.asObservable();
-  }
+    isAdmin() {
+        return this.admin.asObservable();
+    }
 
-  login(): void {
-    this.servicesService.getAuthorizationUrl().subscribe(response => {
-      window.location.href = response.authorization_url;
-    });
-  }
+    getAvatarUrl(): Observable<string> {
+        return this.avatarUrl.asObservable();
+    }
 
-  logout(): void {
-    sessionStorage.clear();
-    localStorage.clear();
+    login(): void {
+        this.servicesService.getAuthorizationUrl().subscribe(response => {
+            window.location.href = response.authorization_url;
+        });
+    }
 
-    this.loggedIn.next(false);
-    this.avatarUrl.next('./assets/icons/person.svg');
-    
-    this.router.navigate(['/']);
-  }
+    logout(): void {
+        sessionStorage.clear();
+        localStorage.clear();
 
-  handleCallback(code: string): void {
-    this.servicesService.postToken(code).subscribe(response => {
-      const userId = response.user_id;
+        this.loggedIn.next(false);
+        this.avatarUrl.next('./assets/icons/person.svg');
 
-      this.servicesService.getUserProfile(userId).subscribe(userInfo => {
-        sessionStorage.setItem('user', JSON.stringify(userInfo));
-        this.loggedIn.next(true);
-        this.avatarUrl.next(userInfo.avatar_url);
         this.router.navigate(['/']);
-      });
-    });
-  }
+    }
+
+    handleCallback(code: string): void {
+        this.servicesService.postToken(code).subscribe(response => {
+            const userId = response.user_id;
+
+            this.servicesService.getUserOsuProfile(userId).subscribe(userInfo => {
+                sessionStorage.setItem('osuUser', JSON.stringify(userInfo));
+                this.loggedIn.next(true);
+                this.avatarUrl.next(userInfo.avatar_url);
+                this.router.navigate(['/']);
+            });
+
+            this.servicesService.getUser(userId).subscribe(userInfo => {
+                sessionStorage.setItem('user', JSON.stringify(userInfo));
+            });
+        });
+    }
 }
