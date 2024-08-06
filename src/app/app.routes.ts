9f969ddc29ea2490a8632a338d5e3e0fc426f55b
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-06 19:51:46 +0200

    comment out manageRequest imports and usages for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { LatestRequestsComponent } from './beatmaps/components/latest-requests/latest-requests.component';
 import { CallbackComponent } from './beatmaps/components/callback/callback.component';
 import { MyRequestsComponent } from './beatmaps/components/my-requests/my-requests.component';
-import {ManageRequestsComponent} from "./beatmaps/components/manage-requests/manage-requests.component";
+// import {ManageRequestsComponent} from "./beatmaps/components/manage-requests/manage-requests.component";
 import {authGuard} from "./beatmaps/auth.guard";
-import {adminGuard} from "./beatmaps/admin.guard";
+// import {adminGuard} from "./beatmaps/admin.guard";
 
 export const routes: Routes = [
     { path: '', component: LatestRequestsComponent },
     { path: 'callback', component: CallbackComponent },
     { path: 'myrequests', component: MyRequestsComponent, canActivate: [authGuard] },
-    { path: 'managerequests', component: ManageRequestsComponent, canActivate: [adminGuard] },
+    // { path: 'managerequests', component: ManageRequestsComponent, canActivate: [adminGuard] },
 ];
 
