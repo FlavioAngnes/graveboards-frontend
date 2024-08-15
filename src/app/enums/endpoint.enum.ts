301c52c9745f2737ad0db9a2d59c59d485c98d81
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-15 03:22:52 +0200

    Move enums to separate folder

---
+++ src/app/enums/endpoint.enum.ts
@@ -0,0 +1,9 @@
+export enum EndpointEnum {
+    BEATMAPS = "api/v1/beatmaps",
+    LEADERBOARDS = "api/v1/leaderboards",
+    LISTINGS = "api/v1/beatmapsets/listings",
+    LOGIN = "api/v1/login",
+    TOKEN = "api/v1/token",
+    USER = "api/v1/users/{userId}",
+    REQUESTS = "api/v1/requests"
+  }
