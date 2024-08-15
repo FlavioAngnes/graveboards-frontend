3ab5673f17fbdc3a16afe0fe7143352ed7d7412c
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-15 03:19:25 +0200

    remove unused fallback

--- src/app/beatmaps/directives/beatmap-thumbnail-fallback.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BeatmapThumbnailFallbackDirective } from './beatmap-thumbnail-fallback.directive';
-
-describe('BeatmapThumbnailFallbackDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BeatmapThumbnailFallbackDirective();
-    expect(directive).toBeTruthy();
-  });
-});
