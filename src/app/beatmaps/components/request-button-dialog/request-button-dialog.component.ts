b4ec29df8670a6e0edd8b5ddf88d91cd43a7f342
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-08-17 14:51:31 +0200

    Fix styling of request dialog

--- src/app/beatmaps/components/request-button-dialog/request-button-dialog.component.ts
+++ src/app/beatmaps/components/request-button-dialog/request-button-dialog.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogRef,
+    MatDialogTitle
+} from '@angular/material/dialog';
+import {ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {CommonModule} from '@angular/common';
 import {MatOption, MatSelect} from "@angular/material/select";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
-  selector: 'app-request-button-dialog',
-  templateUrl: './request-button-dialog.component.html',
-  styleUrls: ['./request-button-dialog.component.scss'],
-  standalone: true,
+    selector: 'app-request-button-dialog',
+    templateUrl: './request-button-dialog.component.html',
+    styleUrls: ['./request-button-dialog.component.scss'],
+    standalone: true,
     imports: [
         MatButtonModule,
         MatInputModule,
@@ -20,29 +27,34 @@ import {MatOption, MatSelect} from "@angular/material/select";
         CommonModule,
         MatSelect,
         MatOption,
+        MatDialogTitle,
+        MatDialogContent,
+        MatDialogActions,
+        MatIcon,
+        MatDialogClose,
     ],
 })
 export class RequestButtonDialogComponent {
-  form: FormGroup;
+    form: FormGroup;
 
-  constructor(
-    private fb: FormBuilder,
-    private dialogRef: MatDialogRef<RequestButtonDialogComponent>
-  ) {
-    this.form = this.fb.group({
-      beatmapsetLink: ['', [Validators.required, Validators.pattern('^(https?:\\/\\/)?osu\\.ppy\\.sh\\/beatmapsets\\/\\d+(#(osu|taiko|fruits|mania)\\/\\d+)?$')]],
-      comment: [''],
-      mvChecked: [false]
-    });
-  }
-
-  onCancel(): void {
-    this.dialogRef.close();
-  }
-
-  onSubmit(): void {
-    if (this.form.valid) {
-      this.dialogRef.close(this.form.value);
+    constructor(
+        private fb: FormBuilder,
+        private dialogRef: MatDialogRef<RequestButtonDialogComponent>
+    ) {
+        this.form = this.fb.group({
+            beatmapsetLink: ['', [Validators.required, Validators.pattern('^(https?:\\/\\/)?osu\\.ppy\\.sh\\/beatmapsets\\/\\d+(#(osu|taiko|fruits|mania)\\/\\d+)?$')]],
+            comment: [''],
+            mvChecked: [false]
+        });
     }
-  }
+
+    onCancel(): void {
+        this.dialogRef.close();
+    }
+
+    onSubmit(): void {
+        if (this.form.valid) {
+            this.dialogRef.close(this.form.value);
+        }
+    }
 }
