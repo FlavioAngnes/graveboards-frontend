f5fe08339c7647249dd3d18dae6e9bb657d141dc
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-11-27 14:03:04 +0100

    Add view type for beatmapsetPanelSkeleton (grid or list)

--- src/components/beatmapsets/beatmapsetPanelSkeleton.tsx
+++ src/components/beatmapsets/beatmapsetPanelSkeleton.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
 
-const BeatmapsetPanelSkeleton = () => {
-    return (
-        <div className="flex flex-col items-start shrink-0 rounded-xl overflow-hidden self-stretch min-w-72 h-64 bg-tertiary-50 dark:bg-tertiary-900">
-            <div className="animate-pulse w-full h-full">
-                <div className="flex h-[9.75rem] bg-tertiary-100 dark:bg-tertiary-750"></div>
-                <div className="flex gap-3 p-2.5 self-stretch min-w-24 relative">
-                    <div className="rounded-full size-10 bg-tertiary-100 dark:bg-tertiary-750"></div>
-                    <div className="flex flex-col gap-1 flex-1 mb-8">
-                        <div className="rounded-xl w-32 h-4 bg-tertiary-100 dark:bg-tertiary-750"></div>
-                        <div className="rounded-xl w-24 h-3 bg-tertiary-100 dark:bg-tertiary-750"></div>
-                        <div className="rounded-xl w-24 h-3 bg-tertiary-100 dark:bg-tertiary-750"></div>
+interface BeatmapsetPanelSkeletonProps {
+    view?: "list" | "grid"
+}
+
+const BeatmapsetPanelSkeleton = ({view}: BeatmapsetPanelSkeletonProps) => {
+    return (view && view === "grid" ? (
+            <div
+                className="flex flex-col items-start shrink-0 rounded-xl overflow-hidden self-stretch min-w-72 h-64 bg-tertiary-50 dark:bg-tertiary-900">
+                <div className="animate-pulse w-full h-full">
+                    <div className="flex h-[9.75rem] bg-tertiary-200 dark:bg-tertiary-750"></div>
+                    <div className="flex gap-3 p-2.5 self-stretch min-w-24 relative">
+                        <div className="rounded-full size-10 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                        <div className="flex flex-col gap-1 flex-1 mb-8">
+                            <div className="rounded-xl w-32 h-4 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            <div className="rounded-xl w-24 h-3 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            <div className="rounded-xl w-24 h-3 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                        </div>
                     </div>
                 </div>
             </div>
+        ) : (
+            <div className="rounded-xl overflow-hidden bg-tertiary-50 dark:bg-tertiary-900 h-24">
+                <div className="flex flex-1 animate-pulse h-full">
+                    <div className="hidden sm:block h-full aspect-video bg-tertiary-200 dark:bg-tertiary-750"></div>
+                    <div className="flex w-full items-center gap-8 p-8 sm:p-4 self-stretch">
+                        <div className="flex flex-col gap-1 w-1/4 xl:w-1/5">
+                            <div className="rounded-xl w-32 h-4 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            <div className="rounded-xl w-24 h-3 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            <div className="block lg:hidden rounded-xl w-24 h-3 bg-tertiary-100 dark:bg-tertiary-750"></div>
+                        </div>
 
-        </div>
+                        <div className="hidden lg:flex items-center gap-2 truncate">
+                            <div className="rounded-full size-10 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            <div className="flex flex-col gap-1">
+                                <div className="rounded-xl w-24 h-3 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                                <div className="rounded-xl w-32 h-4 bg-tertiary-200 dark:bg-tertiary-750"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
     );
 };
 
