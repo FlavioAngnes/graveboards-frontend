7cca7334b904a19f9faaa0d8ee2787e957f27117
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-11-29 18:19:18 +0100

    Add custom hook for beatmapset fetching

---
+++ src/hooks/useBeatmapsets.ts
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+import {BeatmapsetListing} from "@/types/beatmapsets/Beatmapset";
+import {BeatmapsetListingOptions, getBeatmapsets} from "@/services/beatmapsetService";
+import {useSorting} from "@/context/beatmapsets/SortingContext";
+
+const useBeatmapsets = (page: number) => {
+    const [beatmapsets, setBeatmapsets] = useState<BeatmapsetListing[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+    const [hasMore, setHasMore] = useState<boolean>(false);
+
+    const {layersToUse} = useSorting();
+
+    useEffect(() => {
+        const options: BeatmapsetListingOptions = {
+            sortingLayers: layersToUse
+        }
+
+        setLoading(true);
+        setError(null);
+
+        const controller = new AbortController();
+        const {signal} = controller;
+
+        getBeatmapsets(page, options, {signal})
+            .then(data => {
+                if (page === 0) {
+                    setBeatmapsets(data);
+                } else {
+                    setBeatmapsets((prev) => [...prev, ...data]);
+                }
+                setHasMore(!!data.length);
+                setLoading(false);
+            })
+            .catch(e => {
+                setLoading(false);
+                if (signal.aborted) return;
+                setError(e.message);
+            })
+
+        return () => controller.abort();
+    }, [page, layersToUse]);
+
+    return {beatmapsets, loading, error, hasMore}
+}
+
+export default useBeatmapsets;
