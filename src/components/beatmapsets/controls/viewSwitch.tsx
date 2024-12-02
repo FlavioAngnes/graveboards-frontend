e9596feaa14a5a5123403ad7cb2c6c18095f5764
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-12-02 06:14:15 +0100

    Fix styles

--- src/components/beatmapsets/controls/viewSwitch.tsx
+++ src/components/beatmapsets/controls/viewSwitch.tsx
@@ -1,5 +1,6 @@
 import React, {Dispatch, FC, SetStateAction} from "react";
 import {MdGridView, MdOutlineViewAgenda} from "react-icons/md";
+import clsx from "clsx";
 
 interface ViewSwitchProps {
     view: "list" | "grid",
@@ -12,7 +13,8 @@ export const ViewSwitch: FC<ViewSwitchProps> = ({view, setView}) => {
             <div className="block sm:hidden">
                 <button
                     onClick={() => view === 'grid' ? setView('list') : setView('grid')}
-                    className={`p-1 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex sm:hidden items-center border-2 text-primary-500 border-primary-500 justify-center transition-colors duration-300 ease-in-out`}>
+                    className="p-1 size-9 rounded-lg text-primary-500 border-primary-500 hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out"
+                >
                     <MdGridView className={`size-5 ${view === 'grid' ? 'block' : 'hidden'}`}/>
                     <MdOutlineViewAgenda className={`size-5 ${view === 'list' ? 'block' : 'hidden'}`}/>
                 </button>
@@ -20,14 +22,18 @@ export const ViewSwitch: FC<ViewSwitchProps> = ({view, setView}) => {
             <div className="hidden sm:flex gap-2">
                 <button
                     onClick={() => setView('list')}
-                    className={`p-1 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out
-                            ${view === 'list' ? `text-primary-500 border-primary-500` : `text-tertiary-500 border-transparent hover:dark:border-tertiary-900 active:dark:border-tertiary-800`}`}>
+                    className={clsx(
+                        `p-1 size-9 rounded-lg hover:dark:border-tertiary-900 active:dark:border-tertiary-800 hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out`,
+                        view === 'list' ? 'text-primary-500 border-primary-500' : 'text-tertiary-500 border-transparent'
+                    )}>
                     <MdOutlineViewAgenda className="size-5"/>
                 </button>
                 <button
                     onClick={() => setView('grid')}
-                    className={`p-1 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out
-                            ${view === 'grid' ? `text-primary-500 border-primary-500` : `text-tertiary-500 border-transparent hover:dark:border-tertiary-900 active:dark:border-tertiary-800`}`}>
+                    className={clsx(
+                        `p-1 size-9 rounded-lg hover:dark:border-tertiary-900 active:dark:border-tertiary-800 hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out`,
+                        view === 'grid' ? 'text-primary-500 border-primary-500' : 'text-tertiary-500 border-transparent'
+                    )}>
                     <MdGridView className="size-5"/>
                 </button>
             </div>
