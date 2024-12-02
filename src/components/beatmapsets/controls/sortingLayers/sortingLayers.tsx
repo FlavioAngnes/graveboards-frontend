96788241a661a4f0410a88c702b02d85b03751ad
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-12-02 18:34:42 +0100

    Add popover with default layers list

--- src/components/beatmapsets/controls/sortingLayers/sortingLayers.tsx
+++ src/components/beatmapsets/controls/sortingLayers/sortingLayers.tsx
@@ -4,12 +4,14 @@ import SortingLayer from "@/components/beatmapsets/controls/sortingLayers/sortin
 import {Reorder} from 'motion/react';
 import {useSorting} from "@/context/beatmapsets/BeatmapsetListSortingContext";
 import clsx from "clsx";
+import {IoMdInformationCircle, IoMdInformationCircleOutline} from "react-icons/io";
 
 const SortingLayers = () => {
     const [open, setOpen] = useState(false);
 
     const {
         layers,
+        layersToUse,
         nextLayer,
         addLayer,
         removeLayer,
@@ -39,6 +41,8 @@ const SortingLayers = () => {
         }
     }, []);
 
+    const [popoverOpen, setPopoverOpen] = useState(false);
+
     return (
         <>
             <button
@@ -46,87 +50,112 @@ const SortingLayers = () => {
                 onClick={() => setOpen(!open)}
                 className={clsx(
                     `p-1 size-9 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 flex items-center border-2 justify-center transition-colors duration-300 ease-in-out`,
-                    { 'bg-tertiary-100 dark:bg-tertiary-900': open },
+                    {'bg-tertiary-100 dark:bg-tertiary-900': open},
                     canClear ? `text-primary-500 border-primary-500` : `text-tertiary-500 border-transparent hover:dark:border-tertiary-900 active:dark:border-tertiary-800`,
-                    { 'animate-wiggle': canApply && !open }
+                    {'animate-wiggle': canApply && !open}
                 )}
             >
                 <MdImportExport className="size-5"/>
             </button>
             {open && (
-                    <div
-                        ref={sortingRef}
-                        className={
-                            `sm:absolute fixed w-screen h-screen sm:w-auto sm:h-auto top-0 sm:top-12 right-0 p-4 z-10 rounded-none border-0 sm:rounded-xl bg-tertiary-50 dark:bg-tertiary-900 flex flex-col gap-4 transition-colors duration-300 ease-in-out 
+                <div
+                    ref={sortingRef}
+                    className={
+                        `sm:absolute fixed w-screen h-screen sm:w-auto sm:h-auto top-0 sm:top-12 right-0 p-4 z-10 rounded-none border-0 sm:rounded-xl bg-tertiary-50 dark:bg-tertiary-900 flex flex-col gap-4 transition-colors duration-300 ease-in-out 
                             ${canApply ? "border-primary-500 sm:border-2" : "border-tertiary-300 dark:border-tertiary-700 sm:border-[1px]"}`
-                        }
-                    >
-                        <div className="flex gap-1 justify-between">
-                            <div>
-                                <div className="text-black dark:text-white font-semibold flex gap-1 items-center">
-                                    <MdImportExport className="size-4"/>
-                                    Sorting Layers
-                                </div>
-                                <div className="text-tertiary-500 dark:text-tertiary-400 text-sm">
-                                    Drag and drop to reorder the sorting layers.
-                                </div>
+                    }
+                >
+                    <div className="flex gap-1 justify-between">
+                        <div>
+                            <div className="text-black dark:text-white font-semibold flex gap-1 items-center">
+                                <MdImportExport className="size-4"/>
+                                Sorting Layers
+                                {layersToUse.some((layer) => layer.isDefault) && (
+                                    <IoMdInformationCircleOutline
+                                        className="cursor-pointer"
+                                        onMouseEnter={() => setPopoverOpen(true)}
+                                        onMouseLeave={() => setPopoverOpen(false)}
+                                    />
+                                )}
+                                {popoverOpen && (
+                                    <div className="relative">
+                                        <div
+                                            className="absolute z-10 w-64 p-2 bg-white border rounded-lg text-tertiary-500 dark:text-tertiary-400 dark:bg-tertiary-800 dark:border-tertiary-700 right-0 top-full mt-4"
+                                        >
+                                            Sorting applied may not work due to default sorting layers being active in this list.
+                                            <ul className="list-disc pl-4">
+                                                {layersToUse
+                                                    .filter((layer) => layer.isDefault)
+                                                    .map((layer) => (
+                                                    <li key={layer.value}>
+                                                        {layer.value}, {layer.order}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
-                            <button
-                                onClick={() => setOpen(false)}
-                                className={`p-1 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 sm:hidden flex items-center justify-center transition-colors duration-300 ease-in-out`}>
-                                <MdClose className="size-5"/>
-                            </button>
-                        </div>
-                        <div className="flex flex-col gap-4 flex-1">
-                            {canClear && (
-                                <Reorder.Group as="div" onReorder={reorderLayers}
-                                               values={layers.map((layer => layer.value))}
-                                               layout={"position"}
-                                               className="flex flex-col gap-2">
-                                    {layers.map((layer) => (
-                                        <SortingLayer values={layers} value={layer} onChange={updateLayer}
-                                                      onDestroy={removeLayer} key={layer.value}/>
-                                    ))}
-                                </Reorder.Group>
-                            )}
-                            <div className="flex flex-col gap-2">
-                                {
-                                    nextLayer && (
-                                        <button
-                                            onClick={() => addLayer()}
-                                            className="flex items-center gap-1.5 whitespace-nowrap rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 dark:text-tertiary-400 text-tertiary-500 p-2">
-                                            <MdAdd className="size-4"/>
-                                            Add Sorting Layer
-                                        </button>
-                                    )
-                                }
-                                {canClear && (<button
-                                    onClick={() => clearLayers()}
-                                    className="flex items-center gap-1.5 whitespace-nowrap rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 text-primary-400 p-2">
-                                    <MdClose className="size-4"/>
-                                    Clear Sorting
-                                </button>)}
+                            <div className="text-tertiary-500 dark:text-tertiary-400 text-sm">
+                                Drag and drop to reorder the sorting layers.
                             </div>
                         </div>
-                        {
-                            canApply && (
-                                <div className="flex gap-2 self-end sticky bottom-0">
-                                    <button
-                                        onClick={() => undoLayers()}
-                                        className="flex items-center gap-1.5 whitespace-nowrap self-end rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 dark:text-tertiary-400 text-tertiary-500 p-2 px-4">
-                                        <MdUndo className="size-4"/>
-                                        Undo
-                                    </button>
-                                    <button
-                                        onClick={() => applyLayers()}
-                                        className="flex items-center whitespace-nowrap self-end rounded-lg bg-primary-500 text-white p-2 px-4">
-                                        Apply
-                                    </button>
-                                </div>
-
-                            )
-                        }
+                        <button
+                            onClick={() => setOpen(false)}
+                            className={`p-1 rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 sm:hidden flex items-center justify-center transition-colors duration-300 ease-in-out`}>
+                            <MdClose className="size-5"/>
+                        </button>
                     </div>
+                    <div className="flex flex-col gap-4 flex-1">
+                        {canClear && (
+                            <Reorder.Group as="div" onReorder={reorderLayers}
+                                           values={layers.map((layer => layer.value))}
+                                           layout={"position"}
+                                           className="flex flex-col gap-2">
+                                {layers.map((layer) => (
+                                    <SortingLayer values={layers} value={layer} onChange={updateLayer}
+                                                  onDestroy={removeLayer} key={layer.value}/>
+                                ))}
+                            </Reorder.Group>
+                        )}
+                        <div className="flex flex-col gap-2">
+                            {
+                                nextLayer && (
+                                    <button
+                                        onClick={() => addLayer()}
+                                        className="flex items-center gap-1.5 whitespace-nowrap rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 dark:text-tertiary-400 text-tertiary-500 p-2">
+                                        <MdAdd className="size-4"/>
+                                        Add Sorting Layer
+                                    </button>
+                                )
+                            }
+                            {canClear && (<button
+                                onClick={() => clearLayers()}
+                                className="flex items-center gap-1.5 whitespace-nowrap rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 text-primary-400 p-2">
+                                <MdClose className="size-4"/>
+                                Clear Sorting
+                            </button>)}
+                        </div>
+                    </div>
+                    {
+                        canApply && (
+                            <div className="flex gap-2 self-end sticky bottom-0">
+                                <button
+                                    onClick={() => undoLayers()}
+                                    className="flex items-center gap-1.5 whitespace-nowrap self-end rounded-lg hover:bg-tertiary-200 dark:hover:bg-tertiary-800 dark:text-tertiary-400 text-tertiary-500 p-2 px-4">
+                                    <MdUndo className="size-4"/>
+                                    Undo
+                                </button>
+                                <button
+                                    onClick={() => applyLayers()}
+                                    className="flex items-center whitespace-nowrap self-end rounded-lg bg-primary-500 text-white p-2 px-4">
+                                    Apply
+                                </button>
+                            </div>
+
+                        )
+                    }
+                </div>
             )}
         </>
     );
