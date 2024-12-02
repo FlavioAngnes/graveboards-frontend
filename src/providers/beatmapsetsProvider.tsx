f007492138253f794cc814888f8f1ca0d8eaae46
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2024-12-02 06:57:19 +0100

    Add context to beatmapsetsProvider component

--- src/providers/beatmapsetsProvider.tsx
+++ src/providers/beatmapsetsProvider.tsx
@@ -1,27 +1,29 @@
 import {FC, ReactNode} from "react";
 import {SortingProvider} from "@/context/beatmapsets/SortingContext";
 import Beatmapsets from "@/components/beatmapsets/beatmapsets";
-import {BeatmapsetsFilters} from "@/types/beatmapsets/Filters";
 import {SortingLayerOptions} from "@/types/beatmapsets/Sorting";
 import {FiltersProvider} from "@/context/beatmapsets/FiltersContext";
+import {FilterOptions} from "@/types/beatmapsets/Filters";
 
 export const BeatmapsetsProvider: FC<{
     title?: string,
-    defaultFilters?: BeatmapsetsFilters;
+    defaultFilters?: Required<FilterOptions<never>>[];
     defaultSortingLayers?: Required<SortingLayerOptions>[];
     queueId?: number;
     showControls?: boolean;
     children?: ReactNode;
 }> = ({
           title = 'Beatmapsets',
-          //defaultFilters = {},
+          defaultFilters = [],
           defaultSortingLayers = [],
           //queueId,
           showControls = true,
       }) => {
     return (
-        <SortingProvider defaultSortingLayers={defaultSortingLayers}>
-            <Beatmapsets title={title} showControls={showControls}/>
-        </SortingProvider>
+        <FiltersProvider defaultFilters={defaultFilters}>
+            <SortingProvider defaultSortingLayers={defaultSortingLayers}>
+                <Beatmapsets title={title} showControls={showControls}/>
+            </SortingProvider>
+        </FiltersProvider>
     )
 }
