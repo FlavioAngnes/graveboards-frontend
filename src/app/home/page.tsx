e5fbd8e527e2f0a217fe2360a703882e3dcc60f1
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-12 05:37:41 +0100

    Linting

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,15 +3,17 @@ import {BeatmapsetsProvider} from "@/providers/beatmapsetsProvider";
 const Home = async () => {
     return (
         <div className="flex flex-col gap-6">
-            <BeatmapsetsProvider title="Latest Beatmaps" defaultSortingLayers={
-                [
-                    {
-                        value: 'Request.created_at',
-                        order: 'desc',
-                        isDefault: true
-                    }
-                ]
-            }/>
+            <BeatmapsetsProvider
+                title="Latest Beatmaps"
+                defaultSortingLayers={
+                    [
+                        {
+                            value: 'Request.created_at',
+                            order: 'desc',
+                            isDefault: true
+                        }
+                    ]
+                }/>
         </div>
     );
 };
