9166ce6a5fa53216f1e69f21086e100007ca843f
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-13 13:26:36 +0100

    Add Request a Map dialog to requestButton

--- src/components/layout/navbar/requestButton.tsx
+++ src/components/layout/navbar/requestButton.tsx
@@ -10,6 +10,8 @@ interface RequestButtonProps {
 const RequestButton: FC<RequestButtonProps> = ({isSearchExpanded}) => {
     const {isAuthenticated} = useAuth();
 
+    const {ref, onOpen, onClose} = useDialog();
+
     return (
         <>
             {isAuthenticated ? (<button
@@ -37,6 +39,46 @@ const RequestButton: FC<RequestButtonProps> = ({isSearchExpanded}) => {
                     )}
                 </button>
             )}
+            <Dialog title={"Request a Map"} ref={ref} onClose={onClose}>
+                <form className="flex flex-col gap-3 mt-2">
+                    <div className="flex flex-col gap-1">
+                        <span className="font-semibold dark:text-white">
+                            Beatmap Link<span className="text-red-500">*</span>
+                        </span>
+                        <input
+                            className=
+                                'w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700'
+                            type="text"
+                            placeholder="Beatmap Link"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <span className="font-semibold dark:text-white">
+                            BN Queue<span className="text-red-500">*</span>
+                        </span>
+                        <SelectQueues/>
+                    </div>
+
+                    <div className="flex flex-col gap-1">
+                        <span className="font-semibold dark:text-white">
+                            Comment
+                        </span>
+                        <textarea
+                            className=
+                                'w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700'
+                            placeholder="Comment"
+                        />
+                    </div>
+                </form>
+                <footer className="flex justify-end gap-2 mt-2">
+                    <Button
+                        rounded="lg"
+                        className="px-4 py-2"
+                    >
+                        Request
+                    </Button>
+                </footer>
+            </Dialog>
         </>
     );
 };
