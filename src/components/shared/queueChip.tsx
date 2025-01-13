27c67258918f67f3ded4219e2e5961d7a2aa64b3
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-13 13:24:45 +0100

    Add queueChip

---
+++ src/components/shared/queueChip.tsx
@@ -0,0 +1,31 @@
+import React, {FC} from 'react';
+import {GoX} from "react-icons/go";
+import {Queue} from "@/types/queue";
+
+interface QueueChipProps {
+    queue: Queue;
+    removeQueue: (queue: Queue) => void;
+}
+
+const QueueChip: FC<QueueChipProps> = ({queue, removeQueue}) => {
+    return (
+        <div className="flex rounded-full overflow-hidden text-sm tracking-wide">
+            <div className="flex items-center gap-1 py-0.5 pl-1.5 pr-1.5 bg-tertiary-50 dark:bg-tertiary-800 text-black dark:text-white">
+                <div
+                   className="size-4 bg-gray-500 rounded-full bg-cover"
+                   style={{backgroundImage: `url(${queue.display_data.owner_profile.avatar_url})`}}></div>
+                {queue.name}
+            </div>
+            <div
+                onClick={(event) => {
+                    event.stopPropagation();
+                    removeQueue(queue);
+                }}
+                className="flex items-center py-0.5 pr-1.5 bg-tertiary-50 dark:bg-tertiary-800 text-black dark:text-white">
+                <GoX/>
+            </div>
+        </div>
+    );
+};
+
+export default QueueChip;
