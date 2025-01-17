69b59b95482ea3a79bc75894d2f6c23d06e6b90d
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-17 04:50:56 +0100

    Add requestGroup component

---
+++ src/components/requests/requestGroup.tsx
@@ -0,0 +1,55 @@
+import React, {FC, ReactNode, useState} from 'react';
+import {BeatmapsetListing} from "@/types/beatmapsets/beatmapset";
+import {MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp} from "react-icons/md";
+import clsx from "clsx";
+import RequestPanel from "@/components/requests/panels/requestPanel";
+
+interface RequestGroupProps {
+    icon?: ReactNode;
+    title: string;
+    beatmapsets: BeatmapsetListing[];
+    view: 'list' | 'grid';
+}
+
+const RequestGroup: FC<RequestGroupProps> = ({icon, title, beatmapsets, view}) => {
+    const [open, setOpen] = useState(true);
+
+    return (
+        <div className="flex flex-col gap-4">
+            <button
+                onClick={() => setOpen(prev => !prev)}
+                className="flex items-center rounded-lg justify-between p-2.5 hover:dark:bg-tertiary-900 active:dark:bg-tertiary-800 transition-colors duration-300 ease-in-out">
+                <div className="flex items-center gap-2">
+                    {icon && (
+                        <div className="size-5">
+                            {icon}
+                        </div>
+                    )}
+                    <div className="text-2xl font-bold truncate transition-all duration-300 ease-in-out">
+                        {title}
+                    </div>
+                </div>
+                    {open ? (
+                        <MdOutlineKeyboardArrowUp className="size-5"/>
+                    ) : (
+                        <MdOutlineKeyboardArrowDown className="size-5"/>
+                    )}
+            </button>
+
+            {open && (
+                <div
+                    className={clsx(
+                        'gap-4',
+                        view === 'grid' ? `grid grid-cols-[repeat(auto-fill,_minmax(18rem,_1fr))]` : `flex flex-col`
+                    )}
+                >
+                    {beatmapsets.map(beatmapset => (
+                        <RequestPanel beatmapset={beatmapset} view={view} key={beatmapset.id}/>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default RequestGroup;
