3653f50374abc8ef06ab4f4d369f953b68daef99
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-17 15:14:41 +0100

    Move RequestDialog to separate component

--- src/components/layout/navbar/requestButton.tsx
+++ src/components/layout/navbar/requestButton.tsx
@@ -1,11 +1,12 @@
+'use client';
+
 import React, {FC} from 'react';
 import {GoLock} from "react-icons/go";
 import {useAuth} from "@/context/AuthContext";
 import {MdAdd} from "react-icons/md";
-import Dialog from "@/components/shared/dialog";
 import useDialog from "@/hooks/useDialog";
 import Button from "@/components/shared/button";
-import SelectQueues from "@/components/shared/selectQueues";
+import RequestDialog from "@/components/layout/navbar/requestDialog";
 
 interface RequestButtonProps {
     isSearchExpanded: boolean;
@@ -18,80 +19,45 @@ const RequestButton: FC<RequestButtonProps> = ({isSearchExpanded}) => {
 
     return (
         <>
-            {isAuthenticated ? (<Button
-                onClick={onOpen}
-                size="lg"
-                rounded="3xl"
-                className={`shrink-0 ${
-                    isSearchExpanded ? 'min-w-12' : 'lg:min-w-80 min-w-40'
-                }`}>
-                <MdAdd className="size-6"/>
-                {!isSearchExpanded && (
-                    <>
-                        <p className="lg:block hidden">Request a Map</p>
-                        <p className="block lg:hidden">Request</p>
-                    </>
-                )}
-            </Button>) : (
-                <Button
-                    size="lg"
-                    rounded="3xl"
-                    className={`shrink-0 ${
-                        isSearchExpanded ? 'min-w-12' : 'lg:min-w-80 min-w-40'
-                    }`} disabled>
-                    <GoLock className="size-6"/>
-                    {!isSearchExpanded && (
-                        <>
-                            <p className="lg:block hidden">Login to request</p>
-                            <p className="block lg:hidden">Login</p>
-                        </>
-                    )}
-                </Button>
-            )}
-            <Dialog title={"Request a Map"} ref={ref} onClose={onClose}>
-                <form className="flex flex-col gap-3 mt-2">
-                    <div className="flex flex-col gap-1">
-                        <span className="font-semibold dark:text-white">
-                            Beatmap Link<span className="text-red-500">*</span>
-                        </span>
-                        <input
-                            className=
-                                'w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700'
-                            type="text"
-                            placeholder="Beatmap Link"
-                        />
-                    </div>
-                    <div className="flex flex-col gap-1">
-                        <span className="font-semibold dark:text-white">
-                            BN Queue<span className="text-red-500">*</span>
-                        </span>
-                        <SelectQueues/>
-                    </div>
-
-                    <div className="flex flex-col gap-1">
-                        <span className="font-semibold dark:text-white">
-                            Comment
-                        </span>
-                        <textarea
-                            className=
-                                'w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700'
-                            placeholder="Comment"
-                        />
-                    </div>
-                </form>
-                <footer className="flex justify-end gap-2 mt-2">
+            {
+                isAuthenticated ? (
                     <Button
-                        rounded="lg"
-                        className="px-4 py-2"
-                    >
-                        Request
+                        onClick={onOpen}
+                        size="lg"
+                        rounded="3xl"
+                        className={`shrink-0 ${
+                            isSearchExpanded ? 'min-w-12' : 'lg:min-w-80 min-w-40'
+                        }`}>
+                        <MdAdd className="size-6"/>
+                        {!isSearchExpanded && (
+                            <>
+                                <p className="lg:block hidden">Request a Map</p>
+                                <p className="block lg:hidden">Request</p>
+                            </>
+                        )}
                     </Button>
-                </footer>
-            </Dialog>
+                ) : (
+                    <Button
+                        size="lg"
+                        rounded="3xl"
+                        className={`shrink-0 ${
+                            isSearchExpanded ? 'min-w-12' : 'lg:min-w-80 min-w-40'
+                        }`} disabled>
+                        <GoLock className="size-6"/>
+                        {!isSearchExpanded && (
+                            <>
+                                <p className="lg:block hidden">Login to request</p>
+                                <p className="block lg:hidden">Login</p>
+                            </>
+                        )}
+                    </Button>
+                )
+            }
+
+            <RequestDialog ref={ref} onClose={onClose}/>
         </>
     );
 };
 
 export default RequestButton;
-
 
---
+++ src/components/layout/navbar/requestDialog.tsx
@@ -0,0 +1,110 @@
+import React, { forwardRef, useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import { createPortal } from "react-dom";
+import Dialog from "@/components/shared/dialog";
+import SelectQueues from "@/components/shared/selectQueues";
+import Button from "@/components/shared/button";
+import { postRequest } from "@/services/requestService";
+
+interface RequestDialogProps {
+  onClose: () => void;
+}
+
+const RequestDialog = forwardRef<HTMLDialogElement, RequestDialogProps>(
+  ({ onClose }, ref) => {
+    const [beatmapLink, setBeatmapLink] = useState<string>("");
+    const [queues, setQueues] = useState<number[]>([]);
+    const [comment, setComment] = useState<string>("");
+
+    const getBeatmapsetId = (link: string): number => {
+      const match = link.match(/https:\/\/osu.ppy.sh\/beatmapsets\/(\d+)/);
+      return match ? parseInt(match[1], 10) : -1;
+    };
+
+    const handleQueuesChange = (queues: number[]) => {
+      setQueues(queues);
+    };
+
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+      if (!isAuthenticated || !user) return;
+
+      const beatmapsetId = getBeatmapsetId(beatmapLink);
+
+      if (beatmapsetId === -1) {
+        return;
+      }
+
+      queues.forEach((queue) => {
+        postRequest({
+          beatmapset_id: beatmapsetId,
+          queue_id: queue,
+          comment,
+          user_id: user.id,
+          mv_checked: true
+        });
+      });
+
+      onClose();
+    };
+
+    const { user, isAuthenticated } = useAuth();
+
+    if (typeof document === undefined || !isAuthenticated) return null;
+
+    return (
+      createPortal(
+        <Dialog title={"Request a Map"} ref={ref} onClose={onClose}>
+          <form className="flex flex-col gap-3 mt-2" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-1">
+                            <span className="font-semibold dark:text-white">
+                                Beatmap Link<span className="text-red-500">*</span>
+                            </span>
+              <input
+                className=
+                  "w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700"
+                type="url"
+                placeholder="Beatmap Link"
+                value={beatmapLink}
+                onChange={(e) => setBeatmapLink(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col gap-1">
+                            <span className="font-semibold dark:text-white">
+                                BN Queue<span className="text-red-500">*</span>
+                            </span>
+              <SelectQueues onSelect={handleQueuesChange} />
+            </div>
+
+            <div className="flex flex-col gap-1">
+                        <span className="font-semibold dark:text-white">
+                            Comment
+                        </span>
+              <textarea
+                className=
+                  "w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700"
+                placeholder="Comment"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+            </div>
+            <footer className="flex justify-end gap-2 mt-2">
+              <Button
+                rounded="lg"
+                className="px-4 py-2"
+                type="submit"
+              >
+                Request
+              </Button>
+            </footer>
+          </form>
+        </Dialog>,
+        document.body
+      )
+    );
+  }
+);
+
+RequestDialog.displayName = "RequestDialog";
+
+export default RequestDialog;
