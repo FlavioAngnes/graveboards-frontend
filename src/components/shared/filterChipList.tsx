9a1fad8b969d8f7f899de9e99c5e06b8431cec6f
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-17 04:53:58 +0100

    Add FilterChipList component

---
+++ src/components/shared/filterChipList.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from "react";
+import {useFilters} from "@/context/beatmapsets/BeatmapsetListFiltersContext";
+import FilterChip from "@/components/shared/filterChip";
+import {BeatmapsetListFiltersMap} from "@/data/beatmapsets/filters";
+import {FilterOperators} from "@/types/filters";
+
+export const FilterChipList: FC = () => {
+    const {userFilters: filters} = useFilters();
+
+    if (filters.length === 0) return null;
+
+    return (
+        <div className="flex gap-2">
+            {filters.map((filter, index) => (
+                Object.entries(filter.options).map(([key, value]) => (
+                    <FilterChip name={filter.value} key={`${index}-${key}`}
+                                label={BeatmapsetListFiltersMap[filter.value].label} option={{
+                        operation: key as FilterOperators,
+                        value: value
+                    }}/>
+                ))
+            ))}
+        </div>
+    );
+};
