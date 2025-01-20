69f1907c73607c54195199be84bd24ec5b92697f
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-01-20 12:46:31 +0100

    Extract inline functions to variables

--- src/components/beatmapsets/controls/filters/filtersInput.tsx
+++ src/components/beatmapsets/controls/filters/filtersInput.tsx
@@ -21,7 +21,45 @@ const FiltersInput: FC<FiltersInputProps> = ({
     const initialValue = initialFilter ? Object.values(initialFilter.options)[0] : '';
     const initialOperator = initialFilter ? Object.keys(initialFilter.options)[0] as FilterOperators : 'eq';
 
+    const [value, setValue] = React.useState<string>(initialValue);
     const [operator, setOperator] = React.useState<FilterOperators>(initialOperator);
+
+    const handleOperatorChange = () => {
+        setOperator(allowedOperators[(allowedOperators.indexOf(operator) + 1) % allowedOperators.length]);
+
+        if(value.length !== 0) {
+            putFilter({
+                value: name,
+                options: {
+                    [operator]: value
+                }
+            });
+        }
+    }
+
+    const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+
+        if (e.target.value.length === 0) {
+            removeFilter({
+                value: name,
+                options: {
+                    [operator]: e.target.value
+                }
+            });
+
+            return;
+        }
+
+        putFilter(
+            {
+                value: name,
+                options: {
+                    [operator]: e.target.value
+                }
+            }
+        );
+    }
 
     return (
         <div className="flex flex-col gap-1">
@@ -36,38 +74,17 @@ const FiltersInput: FC<FiltersInputProps> = ({
                     className={
                         `size-10 p-2 active:text-black active:dark:text-white rounded-lg hover:bg-tertiary-100 active:bg-tertiary-200 dark:hover:bg-tertiary-800 active:dark:bg-tertiary-700 text-tertiary-400 flex items-center justify-center text-xl gap-1 transition-colors duration-300 ease-in-out`
                     }
-                    onClick={() => setOperator(allowedOperators[(allowedOperators.indexOf(operator) + 1) % allowedOperators.length])}>
+                    onClick={handleOperatorChange}>
                     {operator === 'eq' ? '=' : operator === 'neq' ? '≠' : operator === 'gt' ? '>' : operator === 'lt' ? '<' : operator === 'gte' ? '≥' : '≤'}
                 </button>
                 <input
                     className=
                         'w-full sm:w-auto placeholder-tertiary-500 dark:placeholder-tertiary-400 whitespace-nowrap p-2 rounded-lg dark:bg-tertiary-900 outline-none border-[1px] flex items-center justify-between gap-1 transition-colors duration-300 ease-in-out focus:border-primary-500 focus:bg-tertiary-100 focus:dark:bg-tertiary-800 border-tertiary-300 dark:border-tertiary-700'
-
                     name={name}
                     type="text"
                     placeholder={BeatmapsetListFiltersMap[name].label}
-                    defaultValue={initialValue}
-                    onChange={(e) => {
-                        if (e.target.value.length === 0) {
-                            removeFilter({
-                                value: name,
-                                options: {
-                                    [operator]: e.target.value
-                                }
-                            });
-
-                            return;
-                        }
-
-                        putFilter(
-                            {
-                                value: name,
-                                options: {
-                                    [operator]: e.target.value
-                                }
-                            }
-                        )
-                    }}/>
+                    value={value}
+                    onChange={handleValueChange}/>
             </div>
         </div>
     );
