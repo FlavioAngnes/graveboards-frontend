9edeeb7948be0badcbba98370f81557b9ed3de63
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-02-24 17:51:15 +0100

    remove redundant useEffect

--- src/components/beatmapsets/beatmapsetList.tsx
+++ src/components/beatmapsets/beatmapsetList.tsx
@@ -96,10 +96,6 @@ const BeatmapsetList: FC<BeatmapsetsProps> = ({
 
     //#endregion
 
-    useEffect(() => {
-
-    }, [beatmapsets, view]);
-
     if (error) {
         return (
             <div className="text-center text-2xl font-semibold text-red-500">
