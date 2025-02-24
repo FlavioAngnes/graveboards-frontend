2fc3b5a8105f832044601b8db94b03b1485487b8
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-02-24 17:38:02 +0100

    Replace volume&progress sliders with custom component

--- src/components/shared/beatmapPreviewPlayer.tsx
+++ src/components/shared/beatmapPreviewPlayer.tsx
@@ -1,10 +1,12 @@
-'use client';
+"use client";
 
-import React, {useEffect, useRef, useState} from 'react';
-import {useBeatmapPreview} from "@/context/BeatmapPreviewContext";
-import {MdClose, MdPause, MdPlayArrow, MdVolumeOff, MdVolumeUp} from "react-icons/md";
-import {TimeUtils} from "@/utils/timeUtils";
+import React, { useEffect, useRef, useState } from "react";
+import { useBeatmapPreview } from "@/context/BeatmapPreviewContext";
+import { MdClose, MdPause, MdPlayArrow, MdVolumeOff, MdVolumeUp } from "react-icons/md";
+import { TimeUtils } from "@/utils/timeUtils";
 import clsx from "clsx";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
 
 const BeatmapPreviewPlayer = () => {
     const [open, setOpen] = useState(false);
@@ -13,16 +15,16 @@ const BeatmapPreviewPlayer = () => {
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
 
-    const {src, setSrc} = useBeatmapPreview();
+    const { src, setSrc } = useBeatmapPreview();
 
     useEffect(() => {
-        const storageVolume = localStorage.getItem('volume');
+        const storageVolume = localStorage.getItem("volume");
 
         setVolume(storageVolume ? parseFloat(storageVolume) : 0.5);
     }, []);
 
     useEffect(() => {
-        localStorage.setItem('volume', volume.toString());
+        localStorage.setItem("volume", volume.toString());
 
         if (audioRef.current) {
             audioRef.current.volume = volume;
@@ -47,13 +49,13 @@ const BeatmapPreviewPlayer = () => {
         <div
             className={clsx(
                 `bg-tertiary-100 sm:m-4 z-40 dark:bg-tertiary-900 shadow p-4 sm:rounded-xl fixed bottom-0 right-0 flex gap-4 items-center sm:w-auto w-full`,
-                {"hidden": !src || !open}
-                )}>
+                { "hidden": !src || !open }
+            )}>
             <audio ref={audioRef}
                    onTimeUpdate={() => {
                        if (audioRef.current) {
-                            setCurrentTime(audioRef.current.currentTime);
-                            setDuration(audioRef.current.duration);
+                           setCurrentTime(audioRef.current.currentTime);
+                           setDuration(audioRef.current.duration);
                        }
                    }}
                    onLoadedMetadata={() => {
@@ -71,7 +73,7 @@ const BeatmapPreviewPlayer = () => {
             {
                 audioRef.current && audioRef.current.paused && (
                     <button onClick={() => audioRef.current!.play()}>
-                        <MdPlayArrow className="size-8"/>
+                        <MdPlayArrow className="size-8" />
                     </button>
                 )
             }
@@ -79,7 +81,7 @@ const BeatmapPreviewPlayer = () => {
             {
                 audioRef.current && !audioRef.current.paused && (
                     <button onClick={() => audioRef.current!.pause()}>
-                        <MdPause className="size-8"/>
+                        <MdPause className="size-8" />
                     </button>
                 )
             }
@@ -89,18 +91,22 @@ const BeatmapPreviewPlayer = () => {
                     {TimeUtils.formatTime(Number(currentTime.toFixed()))}
                 </div>
                 <div className="text-tertiary-500">
-                    {isNaN(duration) ? '00:00' : TimeUtils.formatTime(Number(duration.toFixed()))}
+                    {isNaN(duration) ? "00:00" : TimeUtils.formatTime(Number(duration.toFixed()))}
                 </div>
             </div>
 
-            <input type="range" min="0" max="1" step="0.01"
-                   value={isNaN(currentTime / duration) ? 0 : currentTime / duration}
-                   onChange={e => {
-                       if (audioRef.current) audioRef.current.currentTime = parseFloat(e.target.value) * audioRef.current.duration;
-                   }}
-                   disabled={!src}
-                   className={`w-full sm:w-64 ${!src ? 'opacity-50' : ''}`}
-            />
+            <RangeSlider
+                min={0}
+                max={1}
+                step={0.01}
+                value={[0, isNaN(currentTime / duration) ? 0 : currentTime / duration]}
+                onInput={(value) => {
+                    if (audioRef.current) audioRef.current.currentTime = value[1] * audioRef.current.duration;
+                }}
+                disabled={!src}
+                className={`single-thumb w-full sm:w-64 ${!src ? "opacity-50" : ""}`}
+                thumbsDisabled={[true, false]}
+                rangeSlideDisabled={true} />
 
             <div className="flex items-center gap-2">
                 {
@@ -109,25 +115,33 @@ const BeatmapPreviewPlayer = () => {
                             () => {
                                 setVolume(0.1);
                             }
-                        }/>
+                        } />
                     ) : (
                         <MdVolumeUp className="size-6 cursor-pointer" onClick={
                             () => {
                                 setVolume(0);
                             }
-                        }/>
+                        } />
                     )
                 }
-                <input type="range" min="0" max="1" step="0.01" value={volume}
-                       className="w-12 sm:w-24"
-                       onChange={e => setVolume(parseFloat(e.target.value))}/>
+
+                <RangeSlider
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    value={[0, volume]}
+                    onInput={(value) => setVolume(value[1])}
+                    disabled={!src}
+                    className="single-thumb w-12 sm:w-24"
+                    thumbsDisabled={[true, false]}
+                    rangeSlideDisabled={true} />
             </div>
 
             <MdClose className="size-6 cursor-pointer shrink-0" onClick={() => {
                 setOpen(false);
                 audioRef.current!.pause();
                 setSrc(null);
-            }}/>
+            }} />
         </div>
 
     );
