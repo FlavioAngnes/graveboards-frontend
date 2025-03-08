f300dae4b137fe06d51da532ad7029094b9bcc0e
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-03-08 17:26:03 +0100

    Fix login button disabled states

--- src/components/layout/navbar/loginButton.tsx
+++ src/components/layout/navbar/loginButton.tsx
@@ -1,31 +1,42 @@
 "use client";
 
-import { startTransition, useActionState } from "react";
+import { startTransition, useActionState, useState } from "react";
 import { FaCircleNotch } from "react-icons/fa6";
 import { MdPerson } from "react-icons/md";
 import { startOAuth } from "@/actions/auth";
+import { usePathname } from "next/navigation";
+import { useAuth } from "@/context/AuthContext";
 
 const LoginButton = () => {
-    const [state, action, isPending] = useActionState(startOAuth, null);
+    const currentPath = usePathname();
+    const isCallback = currentPath === "/callback";
+
+    const [state, action] = useActionState(startOAuth, null);
+    const [disabled, setDisabled] = useState(false);
 
     if (state?.authorization_url) {
         window.location.href = state.authorization_url;
     }
 
+    const { isLoading } = useAuth();
+
     return (
-        <button
-            className="h-14 px-6 items-center gap-2 rounded-full bg-primary-500 hover:bg-primary-400 active:bg-primary-300 disabled:bg-tertiary-500 shrink-0 hidden sm:flex justify-center transition-all duration-300 ease-in-out"
-            disabled={isPending}
-            onClick={() => startTransition(action)}>
-            {isPending ? (
+        disabled || isCallback || isLoading ? (
+            <div
+                className="h-14 px-4 items-center gap-2 rounded-full bg-tertiary-500 shrink-0 hidden sm:flex justify-center transition-all duration-300 ease-in-out">
                 <FaCircleNotch className="size-6 text-white animate-spin" />
-            ) : (
-                <>
-                    <MdPerson className="size-6 text-white" />
-                    <span className="text-white">Login</span>
-                </>
-            )}
-        </button>
+            </div>
+        ) : (
+            <button
+                className="h-14 px-6 items-center gap-2 rounded-full bg-primary-500 hover:bg-primary-400 active:bg-primary-300 shrink-0 hidden sm:flex justify-center transition-all duration-300 ease-in-out"
+                onClick={() => {
+                    startTransition(action);
+                    setDisabled(true);
+                }}>
+                <MdPerson className="size-6 text-white" />
+                <span className="text-white">Login</span>
+            </button>
+        )
     );
 };
 
