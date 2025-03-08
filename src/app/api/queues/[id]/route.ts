f315e5363e99a64754487264b8d3b1f1cbb5c450
Remigiusz Nowicki <remigiusz.nowicki11@gmail.com>
2025-03-08 17:24:37 +0100

    Refactor /api/queues/[id] route handler

--- src/app/api/queues/[id]/route.ts
+++ src/app/api/queues/[id]/route.ts
@@ -1,7 +1,23 @@
-import { getQueue } from "@/actions/queues";
+import { verifySession } from "@/actions/session";
 
-export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+const { API_URL } = process.env;
+
+export async function GET(req: Request, { params }: { params: Promise<{ id: string }> }) {
     const id = Number((await params).id);
 
-    return Response.json(await getQueue(id));
+    const session = await verifySession();
+
+    if (!session) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+
+    const response = await fetch(`${API_URL}/queues/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${session?.token}`
+        }
+    });
+
+    return new Response(JSON.stringify(await response.json()), { status: response.status });
 }
